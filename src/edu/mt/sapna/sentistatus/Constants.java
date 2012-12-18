cbd3940d7dd924047178c3ccfd8b23a9a7cc6fb3
sapna.negi13@gmail.com <sapna.negi13@gmail.com@bb834532-3268-b7c6-45be-a70dfc0f5be4>
2012-12-18 16:31:29 +0000

    Initial commit of the FB app based on Google App engine, which just lists the friends. It uses restfb.

---
+++ src/edu/mt/sapna/sentistatus/Constants.java
@@ -0,0 +1,9 @@
+package edu.mt.sapna.sentistatus;
+
+
+public class Constants {
+
+	public static final String API_KEY = "181717421968585";
+	public static final String APP_SECRET = "0425e5724e60216f62763114c6f62186";
+	
+}
